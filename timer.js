15894ba4b77194021be49a809ead28802db2639d
HlopakA <faka2@ukr.net>
2024-11-04 13:34:51 +0300

    done_but_check_for_best

--- timer.js
+++ timer.js
@@ -1,49 +1,40 @@
-const counter = document.querySelector("#counter") 
+const counter = document.querySelector("#counter");
+
+
+let intervalId;
 
 
 const buttonOne = document.querySelector("#button-1");
 buttonOne.addEventListener("click", startTimer);
 
-
-
 function startTimer() {
+    if(!intervalId) {
+        intervalId = setInterval(() => {
+            counter.innerHTML++
+        }, 1000)
+    }
 
-    setInterval(() => {
-    counter.innerHTML++
-    }, 1000)
-
+        
 }
 
 
-// 
+const buttonTwo = document.querySelector("#button-2");
+buttonTwo.addEventListener("click", stopTimer);
+
+function stopTimer() {
+    if(intervalId) {
+        clearInterval(intervalId)
+    }
+}
 
 
+const buttonThree = document.querySelector("#button-3");
+buttonThree.addEventListener("click", resetTimer);
 
-
-
-
-
-
-
-// const buttonTwo = document.querySelector("#button-2");
-// buttonTwo.addEventListener("click", stopTimer);
-
-// function stopTimer() {
-//     clearInterval(test)
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// const buttonThree = document.querySelector("#button-3");
-// buttonThree.addEventListener("click", resetTimer);
-
-// function resetTimer() {}
+function resetTimer() {
+    if(intervalId) {
+        clearInterval(intervalId)
+        counter.innerHTML = 0 
+        intervalId = null
+    }
+}
